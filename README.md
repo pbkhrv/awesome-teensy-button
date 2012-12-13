3eeca5774219fb7a3f021b746c378adf3ab93e45
Peter Bakhirev <peter@byteclub.com>
2012-12-13 00:24:20 -0500

    Add detection for 'long press'

--- README.md
+++ README.md
@@ -12,11 +12,14 @@ Based on sample code from <http://pjrc.com/teensy/usb_keyboard.html>
 **What does this do?**
 
 Board poses as a USB keyboard.
-Whenever pin B4 is grounded, board sends CTRL+OPT+CMD+* (numpad asterix) button press.
+You need to hook up the Easy Button's button between pin B4 and GND.
+
+Whenever button is pressed and then released less than 2 seconds later, board sends CTRL+OPT+CMD+numpad1 key press.
+Whenever button is pressed and held for longer than 2 seconds, board sends CTRL+OPT+CMD+numpad0 key press (without waiting for button to be released).
 
 **What can you do with this?**
 
-Use KeyboardMaestro or any other keyboard macro app to do something whenever the Easy button is pressed.
+Use KeyboardMaestro or any other keyboard macro app to do something whenever the button is pressed for short or long periods of time.
 
 **How do you build this?**
 
--- awesome_key.c
+++ awesome_key.c
@@ -1,6 +1,9 @@
-/* Keyboard example for Teensy USB Development Board
+/* Awesome Teensy Button
+ * 
+ * Author: Peter Bakhirev
+ *
+ * based on... Keyboard example for Teensy USB Development Board
  * http://www.pjrc.com/teensy/usb_keyboard.html
- * Copyright (c) 2008 PJRC.COM, LLC
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -27,91 +30,158 @@
 #include <util/delay.h>
 #include "usb_keyboard.h"
 
-#define LED_CONFIG	(DDRD |= (1<<6))
-#define LED_ON		(PORTD &= ~(1<<6))
-#define LED_OFF		(PORTD |= (1<<6))
-#define CPU_PRESCALE(n)	(CLKPR = 0x80, CLKPR = (n))
+#define CPU_PRESCALE(n)  (CLKPR = 0x80, CLKPR = (n))
 
 #define PINB_BIT (1 << 4)
 
-void send_key_press();
+#define FALSE 0
+#define TRUE 1
+
+void send_key_press(uint8_t modifiers, uint8_t key);
+void send_modifiers(uint8_t modifiers);
+
+volatile uint16_t idle_count=0;
+volatile uint8_t long_press_reported = FALSE;
 
 int main(void)
 {
-	uint8_t b;
-	uint8_t b_prev=0xFF;
+  uint8_t b;
+  uint8_t b_prev=0xFF;
+  uint8_t press_started = FALSE;
+  uint8_t press_ended = FALSE;
+  uint8_t local_long_press_happened = FALSE;
 
-	// set for 16 MHz clock
-	CPU_PRESCALE(0);
+  // set for 16 MHz clock
+  // http://www.pjrc.com/teensy/prescaler.html
+  CPU_PRESCALE(0);
 
-	// Configure all port B pins as inputs with pullup resistors.
-	// See the "Using I/O Pins" page for details.
-	// http://www.pjrc.com/teensy/pins.html
-	DDRB = 0x00;
-	PORTB = 0xFF;
+  // Configure all port B pins as inputs with pullup resistors.
+  // See the "Using I/O Pins" page for details.
+  // http://www.pjrc.com/teensy/pins.html
+  DDRB = 0x00;
+  PORTB = 0xFF;
 
-	// Initialize the USB, and then wait for the host to set configuration.
-	// If the Teensy is powered without a PC connected to the USB port,
-	// this will wait forever.
-	usb_init();
-	while (!usb_configured()) /* wait */ ;
+  // Configure timer 0 to generate a timer overflow interrupt every
+  // 256*1024 clock cycles, or approx 61 Hz when using 16 MHz clock
+  // This demonstrates how to use interrupts to implement a simple
+  // inactivity timeout.
+  TCCR0A = 0x00;
+  TCCR0B = 0x05;
+  TIMSK0 = (1<<TOIE0);
 
-	// Wait an extra second for the PC's operating system to load drivers
-	// and do whatever it does to actually be ready for input
-	_delay_ms(1000);
+  // Initialize the USB, and then wait for the host to set configuration.
+  // If the Teensy is powered without a PC connected to the USB port,
+  // this will wait forever.
+  usb_init();
+  while (!usb_configured()) /* wait */ ;
 
-	// Configure timer 0 to generate a timer overflow interrupt every
-	// 256*1024 clock cycles, or approx 61 Hz when using 16 MHz clock
-	// This demonstrates how to use interrupts to implement a simple
-	// inactivity timeout.
+  // Wait an extra second for the PC's operating system to load drivers
+  // and do whatever it does to actually be ready for input
+  _delay_ms(1000);
 
-	while (1) {
-		// read all port B pins
-		b = PINB;
-		// check if any pins are low, but were high previously
-    if (((b & PINB_BIT) == 0) && (b_prev & PINB_BIT) != 0) {
-      send_key_press();
-		}
-    //
-		// now the current pins will be the previous, and
-		// wait a short delay so we're not highly sensitive
-		// to mechanical "bounce".
-		b_prev = b;
-		_delay_ms(5);
-	}
+  // Configure timer 0 to generate a timer overflow interrupt every
+  // 256*1024 clock cycles, or approx 61 Hz when using 16 MHz clock
+  // This demonstrates how to use interrupts to implement a simple
+  // inactivity timeout.
+
+  while (1) {
+    // read all port B pins
+    b = PINB;
+
+    // check if key was just pressed
+    // (if any pins are low, but were high previously)
+    if (!press_started && ((b & PINB_BIT) == 0) && (b_prev & PINB_BIT) != 0) {
+
+      // Disable interrupts to avoid interference with ISR
+      cli();
+      idle_count = 0;
+      long_press_reported = FALSE;
+      // re-enable interrupts
+      sei();
+
+      press_started = TRUE;
+      press_ended = FALSE;
+      b_prev = b;
+
+      // debounce
+      _delay_ms(5);
+    }
+
+    // check if key was released and we haven't registered key press yet
+    // (if any pins are high, but were low previously)
+    if (press_started && !press_ended && ((b & PINB_BIT) != 0) && (b_prev & PINB_BIT) == 0) {
+      press_started = FALSE;
+      press_ended = TRUE;
+      send_key_press(KEY_GUI | KEY_ALT | KEY_SHIFT, KEYPAD_1);
+    }
+
+    // Disable interrupts to avoid interference with ISR
+    cli();
+    // Store value in a non-shared variable so that it's easier to read it
+    local_long_press_happened = long_press_reported;
+    // re-enable interrupts
+    sei();
+
+    // check if key is being held for "long press" time
+    // interrupt handler keeps track of time
+    if (press_started && !press_ended && local_long_press_happened) {
+      press_started = FALSE;
+      press_ended = TRUE;
+      send_key_press(KEY_GUI | KEY_ALT | KEY_SHIFT, KEYPAD_0);
+    }
+
+    // now the current pins will be the previous
+    b_prev = b;
+  }
 }
 
-void send_key_press() {
-  // first, press the GUI key
-  keyboard_modifier_keys = KEY_GUI;
+/*
+ * Simulates USB keybord keys being pressed by human
+ */
+void send_key_press(uint8_t modifiers, uint8_t key)
+{
+  // send modifiers first, no other keys
   keyboard_keys[0] = 0;
   keyboard_keys[1] = 0;
   keyboard_keys[2] = 0;
   keyboard_keys[3] = 0;
   keyboard_keys[4] = 0;
   keyboard_keys[5] = 0;
+
+  send_modifiers(modifiers);
   usb_keyboard_send();
   _delay_ms(50);
 
-  // next, press ALT while still holding GUI
-  keyboard_modifier_keys = KEY_GUI | KEY_ALT;
-  usb_keyboard_send();
-  _delay_ms(50);
- 
-  // next, press SHIFT while still holding ALT and GUI
-  keyboard_modifier_keys = KEY_GUI | KEY_ALT | KEY_SHIFT;
+  // send key
+  keyboard_keys[0] = key;
   usb_keyboard_send();
   _delay_ms(50);
 
-  keyboard_keys[0] = KEYPAD_ASTERIX;
-//  keyboard_keys[0] = KEY_F;
-  usb_keyboard_send();
-  _delay_ms(50);
-
+  // reset all keys
   keyboard_keys[0] = 0;
-  usb_keyboard_send();
-  _delay_ms(50);
-
   keyboard_modifier_keys = 0;
   usb_keyboard_send();
+  _delay_ms(50);
+}
+
+void send_modifiers(uint8_t modifier)
+{
+  int bit;
+  for(bit=0; bit < 8; bit++) {
+    if (modifier & (1 << bit)) {
+      keyboard_modifier_keys |= (1 << bit);
+      usb_keyboard_send();
+      _delay_ms(50);
+    }
+  }
+}
+
+// This interrupt routine is run approx 61 times per second.
+ISR(TIMER0_OVF_vect)
+{
+  idle_count++;
+  if (idle_count > 120) {
+    idle_count = 0;
+    long_press_reported = TRUE;
+  }
 }
